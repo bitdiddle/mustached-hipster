e525b377fa7b2ddd3aedf899ae6162c440947810
Larry Xiao <xiaodi@sjtu.edu.cn>
2014-05-30 14:40:18 +0800

    run.sh fix echo

--- run.sh
+++ run.sh
@@ -26,9 +26,9 @@ if [ "$(uname)" == "Darwin" ]; then
 	echo $PIN_ROOT/pin -t $folder/pincache.dylib $parameter -- tar zcf data1.tgz data1
 	$PIN_ROOT/pin -t $folder/pincache.dylib $parameter -- tar zcf data1.tgz data1
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	echo $PIN_ROOT/pin -t $folder/pincache.dylib $parameter -- tar zcf data1.tgz data1
+	echo $PIN_ROOT/pin -t $folder/pincache.so $parameter -- tar zcf data1.tgz data1
 	$PIN_ROOT/pin -t $folder/pincache.so $parameter -- tar zcf data1.tgz data1
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-	echo $PIN_ROOT/pin -t $folder/pincache.dylib $parameter -- tar zcf data1.tgz data1
+	echo $PIN_ROOT/pin -t $folder/pincache.dll $parameter -- tar zcf data1.tgz data1
     $PIN_ROOT/pin -t $folder/pincache.dll $parameter -- tar zcf data1.tgz data1
 fi
